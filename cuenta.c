f26f1d8a4bdcf3ea540491b02e438f3eff9b9449
babsp <48659083+babsp@users.noreply.github.com>
2019-03-29 21:42:42 -0300

    Update cuenta.c

--- cuenta.c
+++ cuenta.c
@@ -1,5 +1,7 @@
-#include <stdlib.h>
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
 int main(void) {
 printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
 }
- 
