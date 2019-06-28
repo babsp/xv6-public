6c4559b1eb16eac6750c86fcac6f925902491d83
babsp <48659083+babsp@users.noreply.github.com>
2019-06-28 17:05:43 -0400

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -129,6 +129,8 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_getprocs]  sys_getprocs,
+[SYS_addresstranslation] sys_addresstranslation,
+
 };
 
 void
